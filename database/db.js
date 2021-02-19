4bcbe6b9247fd9625b929d4033583a5fb017ace8
chandru19g <chandru1918g@gmail.com>
2021-02-19 17:48:30 +0530

    succesfully

--- database/db.js
+++ database/db.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB = async() => {
-    try{
-        await mongoose.connect('mongodb+srv://foodecom:food123@foodecom.1wriy.mongodb.net/<dbname>?retryWrites=true&w=majority', {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true
-        })
-        console.log('Database connected')
-    }catch(err){
-        console.log(err)
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://foodecom:food123@foodecom.1wriy.mongodb.net/<dbname>?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Database connected succesfully");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
